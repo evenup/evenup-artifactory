f65374cf2b8b2cb8a4b77ad5a99473dadf96e069
AlexRoman <aroman@opentable.com>
2017-05-22 11:44:24 -0700

    storage.prop added for backward compat

--- manifests/config.pp
+++ manifests/config.pp
@@ -25,9 +25,20 @@ class artifactory::config (
   }
 
   if $::artifactory::db_type == 'postgresql' {
-    file {"${::artifactory::home_dir}/etc/db.properties":
+    # push both variants so upgrade to 5.x won't cause problems
+    # artifactory 4.x looks for storage.properties
+    file {"${::artifactory::home_dir}/etc/storage.properties":
       ensure  => file,
       path    => "${::artifactory::home_dir}/etc/storage.properties",
+      content => template('artifactory/storage.properties.pg.erb'),
+      notify  => Class['artifactory::service'],
+      owner   => artifactory,
+      group   => artifactory,
+    }
+    # artifactory 5.x looks for db.properties.
+    file {"${::artifactory::home_dir}/etc/db.properties":
+      ensure  => file,
+      path    => "${::artifactory::home_dir}/etc/db.properties",
       content => template('artifactory/storage.properties.pg.erb'),
       notify  => Class['artifactory::service'],
       owner   => artifactory,
@@ -39,23 +50,35 @@ class artifactory::config (
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  if $::artifactory::config_import_xml {
-    file   {   "${::artifactory::home_dir}/etc/artifactory.config.import.xml":
-      ensure  =>   file,
-      content =>   $::artifactory::config_import_xml,
-      owner   =>   artifactory,
-      group   =>   artifactory,
-    }
-  }
+  # This section suppose to push the template with initial config into artifactory.
+  # After import Artifactory renames the file to bootstrap.xml and create latest.xml
+  # Commented out because didn't find correct way how to pass template with conditional
+  # If bootstrap.xml exists than pass the template. Otherwise skip without errors/warnings.
+  #
+  # exec {"check_presence":
+  #   command => '/bin/true',
+  #   onlyif  => "test -f ${::artifactory::home_dir}/etc/artifactory.config.bootstrap.xml",
+  #   path    => ['/usr/bin','/usr/sbin','/bin','/sbin'],
+  # }
+  #
+  # if $::artifactory::config_import_xml {
+  #   file   { "${::artifactory::home_dir}/etc/artifactory.config.import.xml":
+  #     ensure  => file,
+  #     content => $::artifactory::config_import_xml,
+  #     owner   => artifactory,
+  #     group   => artifactory,
+  #     require => Exec['check_presence'],
+  #   }
+  # }
 
   if $::artifactory::server_xml {
     file  {  "${::artifactory::home_dir}/tomcat/conf/server.xml":
-        ensure    =>  file,
-        owner      =>  artifactory,
-        group      =>  artifactory,
-        mode        =>  '0444',
-        content  =>  $::artifactory::server_xml,
-        notify    =>  Class['artifactory::service'],
+      ensure  => file,
+      owner   => artifactory,
+      group   => artifactory,
+      mode    => '0444',
+      content => $::artifactory::server_xml,
+      notify  => Class['artifactory::service'],
     }
   }
 
